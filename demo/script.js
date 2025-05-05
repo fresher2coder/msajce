077e07d605cc8a7b77a4969262a7cb94d96abfbe
Dineshkumar Thangavel <sendmail2dk@gmail.com>
2025-05-05 12:26:49 +0530

    react-basics: layout example added

---
+++ demo/script.js
@@ -0,0 +1,7 @@
+console.log("Hello, world!");
+console.log("This is a demo script.");
+
+let a = 5;
+let b = 10;
+let sum = a + b;
+console.log("The sum of " + a + " and " + b + " is: " + sum);
---
+++ react/react-basics/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
